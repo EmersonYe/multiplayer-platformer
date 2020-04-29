4d2390ffd871c2826ab1f815f824c3403d12c5c0
unknown <emersonye@gmail.com>
2020-04-28 23:31:45 -0700

    More dev setup.

--- src/server/server.js
+++ src/server/server.js
@@ -2,18 +2,18 @@ const express = require('express');
 
 // Setup and Express server
 const app = express();
-
+app.use(express.static('dist'));
 app.use(express.static('public'));
-const server = app.listen(process.env.PORT || 3000);
+
+// Listen on port
+const port = process.env.PORT || 3000;
+const server = app.listen(port);
+console.log(`Server listening on port ${port}`);
 
 // Setup socket.io
 const io = require('socket.io')(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
 // Listen for socket.io connections
 io.on('connection', socket => {
-    console.log('Player connected!', socket.id);
-})
+  console.log('Player connected!', socket.id);
+});
